d0e949c26e1d82332525a43720d592ba7d52377a
Alex Tarasiuk <antar37@gmail.com>
2022-02-10 19:50:35 -0600

    update to app

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const path = require('path');
-
 const nextPath = path.join(__dirname, 'node_modules', '.bin', 'next');
-
 process.argv.length = 1;
 process.argv.push(nextPath, 'start');
-
+process.env.NODE_ENV = 'production';
+process.env.PORT = 8888
 require(nextPath);
