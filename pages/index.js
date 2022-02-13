d6ad5c39344076e0ff33e56346633b1673b43f99
Alex Tarasiuk <antar37@gmail.com>
2022-02-12 20:05:55 -0600

    ok last time

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Tarasiuk.dev autorestart again
+          Tarasiuk.dev autorestart again ok last time
         </h1>
 
         <p className={styles.description}>
