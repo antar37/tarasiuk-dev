0007a86afd1c85777524361c3fb6f9387fdd1cd2
Alex Tarasiuk <antar37@gmail.com>
2022-02-12 19:53:13 -0600

    testing autorestart

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Tarasiuk.dev
+          Tarasiuk.dev autorestart
         </h1>
 
         <p className={styles.description}>
