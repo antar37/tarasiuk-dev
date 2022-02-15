adfb68e11c88792b8ab8fafa500a0cb2804f0c3f
Alex Tarasiuk <antar37@gmail.com>
2022-02-14 22:32:47 -0600

    lets see what happens

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Tarasiuk.dev do it again
+          Tarasiuk.dev do it again 2
         </h1>
 
         <p className={styles.description}>
