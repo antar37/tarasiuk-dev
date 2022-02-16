f1c1b470b428628dea3b3d57bcbf689b4d8b3f45
Alex Tarasiuk <antar37@gmail.com>
2022-02-15 20:34:09 -0600

    11

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Tarasiuk.dev do it again 10
+          Tarasiuk.dev do it again 11
         </h1>
 
         <p className={styles.description}>
