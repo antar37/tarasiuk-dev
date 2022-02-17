db0402eeda951ccd578f5afeb53abc94503d31e2
Alex Tarasiuk <antar37@gmail.com>
2022-02-17 12:02:14 -0600

    added nathan

--- pages/index.js
+++ pages/index.js
@@ -26,27 +26,17 @@ export default function Home() {
             <p>Master Generalist. Relentless Investigator.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://tarasiuk.dev/joel" className={styles.card}>
             <h2>Joel &rarr;</h2>
             <p>Multi-cultural designer with a flair for User Interfaces</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://tarasiuk.dev/nathan"
             className={styles.card}
           >
             <h2>Nathan &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>The self-taught fullstack wiz-kid.</p>
           </a>
         </div>
       </main>
